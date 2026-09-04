e82ffd87b23dcadd5b2525c32df09377ebac4900
karl2603 <arvindrajkarl@gmail.com>
2026-09-04 23:38:19 +0530

    BookMyDoc

---
+++ src/main/java/com/karl/BookMyDoc/BookMyDocApplication.java
@@ -0,0 +1,13 @@
+package com.karl.BookMyDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karl/BookMyDoc/BookMyDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karl.BookMyDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
