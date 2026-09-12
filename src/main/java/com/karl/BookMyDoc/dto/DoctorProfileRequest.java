ddae1268259715d7a80ad99fb11887a2b5f6ed4a
karl2603 <arvindrajkarl@gmail.com>
2026-09-12 22:00:13 +0530

    User Login, Register DTO

---
+++ src/main/java/com/karl/BookMyDoc/dto/DoctorProfileRequest.java
@@ -0,0 +1,4 @@
+package com.karl.BookMyDoc.dto;
+
+public class DoctorProfileRequest {
+}
---
+++ src/main/java/com/karl/BookMyDoc/dto/LoginRequest.java
@@ -0,0 +1,23 @@
+package com.karl.BookMyDoc.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoginRequest {
+    @NotBlank(message = "Enter email address")
+    @Email(message = "Enter valid email address")
+    private String email;
+
+    @NotBlank(message = "Enter password")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+}
