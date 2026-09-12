b85c606d72a6ddaac52be557f9bbc53da2313448
karl2603 <arvindrajkarl@gmail.com>
2026-09-12 20:47:35 +0530

    User, Doctor Entity

---
+++ src/main/java/com/karl/BookMyDoc/entity/DoctorProfile.java
@@ -0,0 +1,28 @@
+package com.karl.BookMyDoc.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "doctor_profiles")
+public class DoctorProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", unique = true, nullable = false)
+    private User user;
+
+    private String clinicName;
+    private String clinicAddress;
+    private String specialization;
+    private double consultationFee;
+}
---
+++ src/main/java/com/karl/BookMyDoc/entity/User.java
@@ -0,0 +1,26 @@
+package com.karl.BookMyDoc.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+    private String password;
+    private LocalDateTime createdAt;
+}
