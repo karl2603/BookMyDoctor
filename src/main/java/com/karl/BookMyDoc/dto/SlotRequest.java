72e439c089d2331002eccfe95af370db55d89e23
karl2603 <arvindrajkarl@gmail.com>
2026-09-12 22:48:12 +0530

    slot DTOs

--- src/main/java/com/karl/BookMyDoc/dto/SlotRequest.java
+++ src/main/java/com/karl/BookMyDoc/dto/SlotRequest.java
@@ -1,4 +1,20 @@
 package com.karl.BookMyDoc.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class SlotRequest {
+    @NotNull(message = "Enter slot start time")
+    private LocalDateTime startTime;
+    @NotNull(message = "Enter slot end time")
+    private LocalDateTime endTime;
 }
--- src/main/java/com/karl/BookMyDoc/dto/SlotResponse.java
+++ src/main/java/com/karl/BookMyDoc/dto/SlotResponse.java
@@ -1,4 +1,18 @@
 package com.karl.BookMyDoc.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class SlotResponse {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
