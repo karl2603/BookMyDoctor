d8d39b1fd7fe069f42a53789122c7f7869c4caa0
karl2603 <arvindrajkarl@gmail.com>
2026-09-23 12:57:47 +0530

    Spring Security Implemented

--- src/main/java/com/karl/BookMyDoc/security/JwtService.java
+++ src/main/java/com/karl/BookMyDoc/security/JwtService.java
@@ -3,6 +3,7 @@ package com.karl.BookMyDoc.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -11,8 +12,10 @@ import java.util.Date;
 @Service
 @Slf4j
 public class JwtService {
+
     private String SecretKeyString = "122d7e9db54b1fa1315fabfc8a5ab4c2e682d9d512980d83a2976f599e1ed7c3";
     private SecretKey secretKey = Keys.hmacShaKeyFor(SecretKeyString.getBytes());
+
     public String generateToken(String email){
         return Jwts.builder()
                 .subject(email)
@@ -21,4 +24,30 @@ public class JwtService {
                 .signWith(secretKey, Jwts.SIG.HS256)
                 .compact();
     }
+
+    public String extractUsername(String jwtToken){
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(jwtToken)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String jwtToken, UserDetails userDetails){
+        String username = extractUsername(jwtToken);
+        return username.equals(userDetails.getUsername()) && !isExpired(jwtToken);
+    }
+
+    public boolean isExpired(String jwtToken){
+        Date expirationDate = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(jwtToken)
+                .getPayload()
+                .getExpiration();
+
+        return expirationDate.before(new Date());
+    }
+
 }
--- src/main/java/com/karl/BookMyDoc/security/SecurityConfiguration.java
+++ src/main/java/com/karl/BookMyDoc/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.karl.BookMyDoc.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,9 +11,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
     //Custom Security Filer Chain
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -34,7 +40,9 @@ public class SecurityConfiguration {
                                 "/api/auth/login"
                         ).permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
